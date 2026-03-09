bd82e91362a0061fff033dd213a7522ae17c0fea
PP-URC <proyectoprototipico666.com@gmail.com>
2026-03-09 17:32:57 -0600

    Implement data visualization for disease classification
    Added data visualization for disease classification using scatter plot.

--- ml/train.py
+++ ml/train.py
@@ -1,1 +1,26 @@
+import matplotlib.pyplot as plt
+#import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 
+colors = {
+    "healthy": "green",
+    "mastitis": "blue",
+    "anaplasmosis": "red"
+}
+
+df1 = gen_df()
+
+plt.figure()
+for disease, group in df1.groupby("disease"):
+    plt.scatter(
+        group["temp"],
+        group["milk_yield"],
+        label=disease
+    )
+
+plt.xlabel("Temp")
+plt.ylabel("Milk yield")
+plt.legend()
+plt.show()
